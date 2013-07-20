e2154b4cfef59c795681017c0f704d031a831afb
Dan Nuffer <danielnuffer@gmail.com>
2013-07-20 15:08:15 -0600

    Add more packages.

--- ubuntu13.04.sh
+++ ubuntu13.04.sh
@@ -1,3 +1,9 @@
+sudo apt-get install git
+git config --global user.email "danielnuffer@gmail.com"
+git config --global user.name "Dan Nuffer"
+git config --global push.default matching
+
 sudo chmod +x /usr/share/doc/git/contrib/subtree/git-subtree.sh
 sudo ln -s /usr/share/doc/git/contrib/subtree/git-subtree.sh /usr/lib/git-core/git-subtree
 
+sudo apt-get install mono-mcs r-base octave openjdk-7-jdk nodejs clisp gobjc gobjc++ gnustep-devel php5 clojure1.4 build-essential ruby ruby-dev python scala coffeescript erlang
